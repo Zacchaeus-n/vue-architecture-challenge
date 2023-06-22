d2672a30c09a81c953ac92411175a895ee6d2192
Zacchaeus Napuo <zacchaeus.napuo@amalitech.org>
2023-06-22 13:53:43 +0000

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports =  {
-    "root": true,
-    "env": {
-      "node": true
-    },
-    "extends": [
-      "plugin:vue/vue3-essential",
-      "eslint:recommended"
-    ],
-    "parserOptions": {
-      "parser": "@babel/eslint-parser"
-    },
-    "rules": {
-      "no-unused-vars": process.env.NODE_ENV !== "production" ? "warn" : "error",
-    }
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
+  ],
+
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+
+  rules: {
+    "no-unused-vars": process.env.NODE_ENV !== "production" ? "warn" : "error",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
+}
